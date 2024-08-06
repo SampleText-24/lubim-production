f4db8964baade4bb4dc544e9b139a1e3bc635ce6
Alexey Sokolov <Sample24Text@gmail.com>
2024-08-06 13:28:17 +0700

    Refactor Webpack config: Decompose into separate modules for loaders, plugins, and resolvers

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,36 +1,22 @@
 import path from 'path';
-// const path = require('path');
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
 import webpack from 'webpack';
-// const webpack = require('webpack');
 
-const config: webpack.Configuration = {
-    mode: 'development',
+import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
+import {BuildPaths} from "./config/build/types/config";
+
+const paths: BuildPaths = {
     entry: path.resolve(__dirname, 'src', 'index.ts'),
-    output: {
-        filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'build'),
-        clean: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    plugins: [
-        new webpack.ProgressPlugin(),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'public', 'index.html')
-        }),
-    ],
-};
+    html: path.resolve(__dirname, 'public', 'index.html'),
+    build: path.resolve(__dirname, 'build')
+}
+
+const mode = 'development';
+const isDev = mode === 'development';
+
+const config: webpack.Configuration = buildWebpackConfig({
+    mode: "development",
+    paths,
+    isDev
+});
 
 export default config;
