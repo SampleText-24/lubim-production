e0e917570422f09a5a7a7b33e2a358e557c26a1b
Alexey Sokolov <Sample24Text@gmail.com>
2024-08-16 18:42:11 +0700

    chore: add Prettier with configuration for consistent code formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import i18next from "eslint-plugin-i18next";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 
 export default [
@@ -11,6 +12,7 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  eslintPluginPrettierRecommended,
   {
     plugins: {
       i18next, // Подключает плагин для работы с i18next
@@ -22,18 +24,11 @@ export default [
           extensions: ['.js', '.jsx', '.tsx'], // Разрешает JSX в указанных типах файлов
         },
       ],
-      indent: [2, 4], // Устанавливает отступы в 4 пробела
       "react/react-in-jsx-scope": "off", // Отключает требование импортировать React в файлах с JSX (не нужно с React 17+)
+      "react/jsx-max-props-per-line": ["error", { maximum: 3 }], // Ограничивает максимальное количество свойств в одной строке
       "no-unused-vars": "off", // Отключает правило о неиспользуемых переменных (для лучшей работы с TypeScript)
       "@typescript-eslint/no-unused-vars": "warn", // Включает предупреждения для неиспользуемых переменных в TypeScript
-      "max-len": [
-        "error",
-        {
-          code: 131, // Устанавливает максимальную длину строки в 131 символ
-          ignoreComments: true, // Игнорирует длину строк в комментариях
-        },
-      ],
       "i18next/no-literal-string": 2, // Запрещает строки без перевода в JSX
     }
-  }
+  },
 ];
