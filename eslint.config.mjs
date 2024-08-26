3137d7585ee95ac73062fc600b4b73bafacea51b
Alexey Sokolov <Sample24Text@gmail.com>
2024-08-26 13:50:41 +0700

    chore: disable i18next/no-literal-string rule in ESLint for test files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,48 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import i18next from "eslint-plugin-i18next";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import i18next from 'eslint-plugin-i18next';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  eslintPluginPrettierRecommended,
-  {
-    plugins: {
-      i18next, // Подключает плагин для работы с i18next
-    },
-    rules: {
-      "react/jsx-filename-extension": [
-        2,
-        {
-          extensions: ['.js', '.jsx', '.tsx'], // Разрешает JSX в указанных типах файлов
+    { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    eslintPluginPrettierRecommended,
+    {
+        plugins: {
+            i18next, // Подключает плагин для работы с i18next
         },
-      ],
-      "react/react-in-jsx-scope": "off", // Отключает требование импортировать React в файлах с JSX (не нужно с React 17+)
-      "react/prop-types": "off", // Отключает требование импортировать PropTypes в файлах с JSX
-      "react/jsx-max-props-per-line": ["error", { maximum: 3 }], // Ограничивает максимальное количество свойств в одной строке
-      "no-unused-vars": "off", // Отключает правило о неиспользуемых переменных (для лучшей работы с TypeScript)
-      "@typescript-eslint/no-unused-vars": "warn", // Включает предупреждения для неиспользуемых переменных в TypeScript
-      "i18next/no-literal-string": 2, // Запрещает строки без перевода в JSX
+        rules: {
+            'react/jsx-filename-extension': [
+                2,
+                {
+                    extensions: ['.js', '.jsx', '.tsx'], // Разрешает JSX в указанных типах файлов
+                },
+            ],
+            'react/react-in-jsx-scope': 'off', // Отключает требование импортировать React в файлах с JSX (не нужно с React 17+)
+            'react/prop-types': 'off', // Отключает требование импортировать PropTypes в файлах с JSX
+            'react/jsx-max-props-per-line': ['error', { maximum: 3 }], // Ограничивает максимальное количество свойств в одной строке
+            'no-unused-vars': 'off', // Отключает правило о неиспользуемых переменных (для лучшей работы с TypeScript)
+            '@typescript-eslint/no-unused-vars': 'warn', // Включает предупреждения для неиспользуемых переменных в TypeScript
+            'i18next/no-literal-string': [
+                'error',
+                {
+                    markupOnly: true,
+                    ignoreAttribute: ['data-testid'],
+                },
+            ], // Запрещает строки без перевода в JSX
+        }
+    },
+    // overrides для тестов (для лучшей работы с i18next)
+    {
+        files: ['**/src/**/*.test.{ts,tsx}'],
+        rules: {
+            'i18next/no-literal-string': 'off',
+        }
     }
-  },
 ];
