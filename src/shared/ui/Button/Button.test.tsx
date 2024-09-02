013ed83edef6b14f0aeb991a21b10642be22c444
Alexey Sokolov <sample24text@gmail.com>
2024-09-02 15:14:03 +0700

    feat: enhance button component with new themes, sizes, states; update stories and tests

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -1,21 +1,33 @@
 import { screen } from '@testing-library/react';
-import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { Button, ButtonTheme, ButtonSize } from 'shared/ui/Button/Button';
 import { renderWithTranslation } from 'shared/config/tests/renderWithTranslation/renderWithTranslation';
 
-describe('button', () => {
-    // Отображение кнопки с текстом по умолчанию
+describe('Button Component', () => {
+    // Тест на рендер кнопки с текстом по умолчанию
     it('renders with default theme', () => {
         renderWithTranslation(<Button>Click me</Button>);
         expect(screen.getByText('Click me')).toBeInTheDocument();
     });
 
-    // Отображение кнопки с прозрачной темой
+    // Тест на рендер кнопки с темой CLEAR
     it('renders with clear theme', () => {
         renderWithTranslation(<Button theme={ButtonTheme.CLEAR}>Click me</Button>);
         expect(screen.getByText('Click me')).toBeInTheDocument();
     });
 
-    // Отображение кнопки с кастомным содержимым
+    // Тест на рендер кнопки с темой OUTLINE
+    it('renders with outline theme', () => {
+        renderWithTranslation(<Button theme={ButtonTheme.OUTLINE}>Click me</Button>);
+        expect(screen.getByText('Click me')).toBeInTheDocument();
+    });
+
+    // Тест на рендер кнопки с темой FILLED
+    it('renders with filled theme', () => {
+        renderWithTranslation(<Button theme={ButtonTheme.FILLED}>Click me</Button>);
+        expect(screen.getByText('Click me')).toBeInTheDocument();
+    });
+
+    // Тест на рендер кнопки с кастомным содержимым
     it('renders with custom children', () => {
         renderWithTranslation(
             <Button>
@@ -25,11 +37,41 @@ describe('button', () => {
         expect(screen.getByText('Custom children')).toBeInTheDocument();
     });
 
-    // Вызов обработчика onClick при клике на кнопку
+    // Тест на вызов обработчика onClick при клике на кнопку
     it('calls onClick handler when clicked', () => {
         const onClick = jest.fn();
         renderWithTranslation(<Button onClick={onClick}>Click me</Button>);
         screen.getByText('Click me').click();
         expect(onClick).toHaveBeenCalledTimes(1);
+    });
+
+    // Тест на рендер кнопки размером SMALL
+    it('renders with small size', () => {
+        renderWithTranslation(<Button size={ButtonSize.S}>Click me</Button>);
+        expect(screen.getByText('Click me')).toHaveStyle('font-size: var(--font-size-s)');
+    });
+
+    // Тест на рендер кнопки размером MEDIUM
+    it('renders with medium size', () => {
+        renderWithTranslation(<Button size={ButtonSize.M}>Click me</Button>);
+        expect(screen.getByText('Click me')).toHaveStyle('font-size: var(--font-size-m)');
+    });
+
+    // Тест на рендер кнопки размером LARGE
+    it('renders with large size', () => {
+        renderWithTranslation(<Button size={ButtonSize.L}>Click me</Button>);
+        expect(screen.getByText('Click me')).toHaveStyle('font-size: var(--font-size-l)');
+    });
+
+    // Тест на рендеринг кнопки в состоянии disabled
+    it('renders as disabled', () => {
+        renderWithTranslation(<Button disabled>Click me</Button>);
+        expect(screen.getByRole('button')).toBeDisabled();
+    });
+
+    // Тест на рендеринг кнопки с шириной 100% (fullWidth)
+    it('renders with full width', () => {
+        renderWithTranslation(<Button fullWidth>Click me</Button>);
+        expect(screen.getByRole('button')).toHaveClass('fullWidth');
     });
 });
--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -5,13 +5,23 @@ import React, { FC, ReactNode } from 'react';
 export enum ButtonTheme {
     CLEAR = 'clear',
     OUTLINE = 'outline',
+    FILLED = 'filled',
+}
+
+export enum ButtonSize {
+    S = 'small',
+    M = 'medium',
+    L = 'large',
 }
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     className?: string;
     theme?: ButtonTheme;
+    size?: ButtonSize;
     children?: ReactNode;
     textAnimation?: boolean;
+    disabled?: boolean;
+    fullWidth?: boolean;
 }
 
 /**
@@ -20,7 +30,10 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
  * @param className - Дополнительный класс для кнопки.
  * @param children - Вложенные элементы отображаемые в кнопке.
  * @param theme - Тема кнопки (по умолчанию CLEAR).
+ * @param size - Размер кнопки (S, M, L).
  * @param textAnimation - Анимация текста при наведении (по умолчанию false).
+ * @param disabled - Флаг отключения кнопки.
+ * @param fullWidth - Флаг, позволяющий установить ширину кнопки на 100%.
  * @param otherProps - Остальные свойства кнопки, передаваемые в элемент.
  *
  * @return Компонент кнопки.
@@ -28,16 +41,23 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 export const Button: FC<ButtonProps> = ({
     className,
     children,
+    size = ButtonSize.M,
     theme = ButtonTheme.CLEAR,
     textAnimation = false,
+    disabled,
+    fullWidth,
     ...otherProps
 }: ButtonProps) => {
+    const mods: Record<string, boolean> = {
+        [cls.textAnimation]: textAnimation,
+        [cls.disabled]: disabled,
+        [cls.fullWidth]: fullWidth,
+    };
+
     return (
         <button
-            className={classNames(cls.btn, { [cls.textAnimation]: textAnimation }, [
-                className,
-                cls[theme],
-            ])}
+            disabled={disabled}
+            className={classNames(cls.btn, mods, [className, cls[size], cls[theme]])}
             {...otherProps}
         >
             <span>{children}</span>
